5b7616ab39bc2ba37a0e65684bf541a73e8b6f48
Arpit Dhakad <arpitdhakad25@gmail.com>
2026-07-08 11:43:25 +0530

    Update CALCULATOR_GUI.py

--- CALCULATOR_GUI.py
+++ CALCULATOR_GUI.py
@@ -30,73 +30,73 @@ def equal():
         entry.insert(tk.END,"error")
 
 
-button1 = tk.Button(window,text = "+",command=lambda:click("+"),width=7,height=3)
+buttonadd = tk.Button(window,text = "+",command=lambda:click("+"),width=7,height=3)
 
-button1.grid(row=1 , column=1)
-
-
-
-button2 = tk.Button(window,text = "-",command=lambda:click("-"),width=7,height=3)
-
-button2.grid(row=1 ,column=2)
+buttonadd.grid(row=1 , column=1)
 
 
 
-button3 = tk.Button(window,text = "*",command=lambda:click("*"),width=7,height=3)
+buttonsub = tk.Button(window,text = "-",command=lambda:click("-"),width=7,height=3)
 
-button3.grid(row= 1, column=3)
+buttonsub.grid(row=1 ,column=2)
+
+
+
+buttonmult = tk.Button(window,text = "*",command=lambda:click("*"),width=7,height=3)
+
+buttonmult.grid(row= 1, column=3)
     
-button4 = tk.Button(window,text = "/",command=lambda:click("/"),width=7,height=3)
+buttondiv = tk.Button(window,text = "/",command=lambda:click("/"),width=7,height=3)
 
-button4.grid(row=2 , column=4)
+buttondiv.grid(row=2 , column=4)
 
-button5 = tk.Button(window,text = "=",command=equal,width=7,height=3)
+buttoneq = tk.Button(window,text = "=",command=equal,width=7,height=3)
 
-button5.grid(row=4 , column=4)
+buttoneq.grid(row=4 , column=4)
 
-button6 = tk.Button(window,text = "c",command=clear,width=7,height=3)
+buttoncl = tk.Button(window,text = "c",command=clear,width=7,height=3)
 
-button6.grid(row=3 , column=4)
+buttoncl.grid(row=3 , column=4)
 
-button7 = tk.Button(window,text = "0",command=lambda:click("0"),width=7,height=3)
+button0 = tk.Button(window,text = "0",command=lambda:click("0"),width=7,height=3)
 
-button7.grid(row=5 , column=2)
+button0.grid(row=5 , column=2)
 
-button8 = tk.Button(window,text = "1",command=lambda:click("1"),width=7,height=3)
+button1 = tk.Button(window,text = "1",command=lambda:click("1"),width=7,height=3)
 
-button8.grid(row=2 , column=1)
+button1.grid(row=2 , column=1)
 
-button9 = tk.Button(window,text = "2",command=lambda:click("2"),width=7,height=3)
+button2 = tk.Button(window,text = "2",command=lambda:click("2"),width=7,height=3)
 
-button9.grid(row=2 , column=2)
+button2.grid(row=2 , column=2)
 
-button10 = tk.Button(window,text = "3",command=lambda:click("3"),width=7,height=3)
+button3 = tk.Button(window,text = "3",command=lambda:click("3"),width=7,height=3)
 
-button10.grid(row=2 , column=3)
+button3.grid(row=2 , column=3)
 
-button11 = tk.Button(window,text = "4",command=lambda:click("4"),width=7,height=3)
+button4 = tk.Button(window,text = "4",command=lambda:click("4"),width=7,height=3)
 
-button11.grid(row=3 , column=1)
+button4.grid(row=3 , column=1)
 
-button12 = tk.Button(window,text = "5",command=lambda:click("5"),width=7,height=3)
+button5 = tk.Button(window,text = "5",command=lambda:click("5"),width=7,height=3)
 
-button12.grid(row=3 , column=2)
+button5.grid(row=3 , column=2)
 
-button13 = tk.Button(window,text = "6",command=lambda:click("6"),width=7,height=3)
+button6 = tk.Button(window,text = "6",command=lambda:click("6"),width=7,height=3)
 
-button13.grid(row=3 , column=3)
+button6.grid(row=3 , column=3)
 
-button14 = tk.Button(window,text = "7",command=lambda:click("7"),width=7,height=3)
+button7 = tk.Button(window,text = "7",command=lambda:click("7"),width=7,height=3)
 
-button14.grid(row=4 , column=1)
+button7.grid(row=4 , column=1)
 
-button15 = tk.Button(window,text = "8",command=lambda:click("8"),width=7,height=3)
+button8 = tk.Button(window,text = "8",command=lambda:click("8"),width=7,height=3)
 
-button15.grid(row=4 , column=2)
+button8.grid(row=4 , column=2)
 
-button16 = tk.Button(window,text = "9",command=lambda:click("9"),width=7,height=3)
+button9 = tk.Button(window,text = "9",command=lambda:click("9"),width=7,height=3)
 
-button16.grid(row=4 , column=3)
+button9.grid(row=4 , column=3)
 
 
 
